86be6f97f9afe8ee4475b6989e475d07015ad180
margo <margo.bozova@gmail.com>
2018-01-19 11:27:25 +0200

    Add get, post, put routes

--- REST/server/routes/book.js
+++ REST/server/routes/book.js
@@ -7,5 +7,34 @@ router.post('/', async (req, res) => {
   res.send(await Book.create(req.body));
 });
 
+router.get('/:bookId', async(req, res) => {
+  await Book.findById(req.params.bookId, (err, book) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(book);
+    }
+  });
+});
+
+router.put('/:bookId', async(req, res) => {
+  await Book.findByIdAndUpdate(req.params.bookId, req.body, (err, book) => {
+    if(err) {
+      res.send(err);
+    } else {
+      res.send(`${book._id} book was changed`);
+    }
+  })
+});
+
+router.delete('/:bookId', async(req, res) => {
+  await Book.findByIdAndRemove(req.params.bookId, (err, book) => {
+    if(err) {
+      res.send(err);
+    } else {
+      res.send(`${book._id} book was deleted`);
+    }
+  })
+});
 
 export default router;
